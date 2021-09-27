eefc8463fb1475159114fc503f2cc89e89df3562
BanVacc <mlgnikitka@gmail.com>
2021-09-27 17:16:03 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.on('connection', client => {
+  client.on('message', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
+});
+server.listen(3000);
